d1012a78be6da6e479a2dae144f3d751dd1bc622
Ushinji <siguma971@gmail.com>
2018-06-01 14:20:16 +0900

    本番環境と開発環境で読み込むassetsファイルのパスを切り替えるヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def assets_path(path)
+    return webpack_dev_server_path(path) if Rails.env.development? || Rails.env.test?
+    manifest = Rails.application.config.assets_manifest
+    path = manifest[path] if manifest && manifest[path].present?
+    "/dist/#{path}"
+  end
+
+  private
+  def webpack_dev_server_path(path)
+    "http://localhost:3500/#{path}"
+  end
 end
