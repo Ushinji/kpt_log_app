71351b8b49e9c35d9bfc0d3f83d60560111bbb33
Ushinji <siguma971@gmail.com>
2018-06-26 11:15:34 +0900

    ApolloProviderの位置を修正

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,59 +1,32 @@
 import React from 'react';
-import { ApolloProvider, Query } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { Query } from 'react-apollo';
 import Queries from '../../queries';
 import AddKptLog from '../organisms/AddKptLog';
 
-const httpLink = createHttpLink({
-  uri: '/api/graphql',
-  credentials: 'include',
-});
+const Dashboard = () => (
+  <div>
+    <Query query={Queries.GET_KPT_LOGS}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading... </p>;
+        if (error) return <p>Error :( </p>;
+        console.log(data);
 
-const authLink = setContext((_, { headers }) => ({
-  headers: {
-    ...headers,
-    'Content-Type': 'application/json',
-    'X-CSRF-Token': window.csrfToken,
-  },
-}));
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-const Dashboard = () => {
-  const kptLogItems = keptLogs => (
-    <div className="test">
-      <div>KPT履歴</div>
-      {keptLogs.map(kptLog => (
-        <ul key={`key-keptlog-${kptLog.id}`}>
-          <li key={`key-keptlog-keep-${kptLog.id}`}>{kptLog.keep}</li>
-          <li key={`key-keptlog-problem-${kptLog.id}`}>{kptLog.problem}</li>
-          <li key={`key-keptlog-try-${kptLog.id}`}>{kptLog.try}</li>
-        </ul>
-      ))}
-    </div>
-  );
-
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        <Query query={Queries.GET_KPT_LOGS}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-
-            return <div>{kptLogItems(data.kpt_logs)}</div>;
-          }}
-        </Query>
-        <AddKptLog />
-      </div>
-    </ApolloProvider>
-  );
-};
+        return (
+          <div>
+            <div>KPT履歴                         </div>
+            {data.kpt_logs.map(kptLog => (
+              <ul key={`key-${kptLog.id}`}>
+                <li key={`key-keep-${kptLog.id}`}>{kptLog.keep}                </li>
+                <li key={`key-problem-${kptLog.id}`}>{kptLog.problem}                </li>
+                <li key={`key-try-${kptLog.id}`}>{kptLog.try}                </li>
+              </ul>
+            ))}
+          </div>
+        );
+      }}
+    </Query>
+    <AddKptLog />
+  </div>
+);
 
 export default Dashboard;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = createHttpLink({
+  uri: '/api/graphql',
+  credentials: 'include',
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': window.csrfToken,
+  },
+}));
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept('./App', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>,
+      document.getElementById('root')
+    );
   });
 }
