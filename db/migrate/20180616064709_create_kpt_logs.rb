56dfa426a53ad7bb89cb3f8d368a1cc5bb33d5f9
Ushinji <siguma971@gmail.com>
2018-07-07 11:21:49 +0900

    Migrationファイルでのテーブル作成にて、DEFAULT CHARSETをutf8mb4とした

--- db/migrate/20180616064709_create_kpt_logs.rb
+++ db/migrate/20180616064709_create_kpt_logs.rb
@@ -1,6 +1,6 @@
 class CreateKptLogs < ActiveRecord::Migration[5.2]
   def change
-    create_table :kpt_logs do |t|
+    create_table :kpt_logs, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4'  do |t|
       t.references :user, null: false
       t.text :keep, null: false
       t.text :problem, null: false
