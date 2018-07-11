7e31c392d6c36263ea6fbeab295b30aa6a2e1138
Ushinji <siguma971@gmail.com>
2018-07-11 20:35:44 +0900

    ヘッダー、サイドメニューのレイアウト作成

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -43,24 +43,33 @@ class Dashboard extends React.Component {
     );
 
     return (
-      <div>
-        <Query query={Queries.GET_KPT_LOGS}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
+      <div className="container">
+        <div className="container--header">KPTLog</div>
+        <div className="container--body">
+          <div className="side-menu">
+            <button
+              onClick={this.openModal}
+              className="side-menu--item"
+              type="button"
+            >
+              <AddKptLogModal
+                isOpen={modalIsOpen}
+                closeModal={this.closeModal}
+                contentLabel="AddKPTLogModal"
+              />
+              モーダルを開く
+            </button>
+            <div className="side-menu--item">作成履歴</div>
+          </div>
+          <Query query={Queries.GET_KPT_LOGS}>
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error :(</p>;
 
-            return <div>{kptLogItems(data.kpt_logs)}</div>;
-          }}
-        </Query>
-
-        <AddKptLogModal
-          isOpen={modalIsOpen}
-          closeModal={this.closeModal}
-          contentLabel="AddKPTLogModal"
-        />
-        <button type="button" onClick={this.openModal}>
-          モーダルを開く
-        </button>
+              return kptLogItems(data.kpt_logs);
+            }}
+          </Query>
+        </div>
       </div>
     );
   }
