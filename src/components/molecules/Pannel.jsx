d48baf0ba57ab0774c6de019ec6b9783eacc2653
Ushinji <siguma971@gmail.com>
2018-07-12 21:42:03 +0900

    KPTログの詳細画面のデザイン実装

--- src/components/molecules/Pannel.jsx
+++ src/components/molecules/Pannel.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment-timezone';
 
-const Pannel = ({ kptLog }) => (
-  <div className="pannel">
-    <div className="pannel__header">
-      {moment(kptLog.created_at).format('YYYY/MM/DD HH:MM')}
-    </div>
-    <div className="pannel__body">
-      <div className="pannel__body__item">
-        <div className="pannel__body__item__title">Keep </div>
-        <div className="pannel__body__item__content">{kptLog.keep} </div>
-      </div>
-      <div className="pannel__body__item">
-        <div className="pannel__body__item__title">Problem </div>
-        <div className="pannel__body__item__content">{kptLog.problem} </div>
-      </div>
-      <div className="pannel__body__item">
-        <div className="pannel__body__item__title">Try </div>
-        <div className="pannel__body__item__content">{kptLog.try} </div>
+const Pannel = ({ title, content, additionalStyle }) => (
+  <div className={`pannel ${additionalStyle}`}>
+    <div className="pannel--header">{title}</div>
+    <div className="pannel--body">
+      <div className="pannel--body--item">
+        <div className="pannel--body--item--content">{content}</div>
       </div>
     </div>
   </div>
 );
 
 Pannel.propTypes = {
-  kptLog: PropTypes.instanceOf(Object).isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  additionalStyle: PropTypes.string.isRequired,
 };
 
 export default Pannel;
--- src/components/organisms/KptLogDetatil.jsx
+++ src/components/organisms/KptLogDetatil.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
+import Pannel from '../molecules/Pannel';
 
 moment.updateLocale('ja', {
   weekdaysShort: ['日', '月', '火', '水', '木', '金', '土'],
@@ -11,7 +12,23 @@ const KptLogDetatil = ({ kptLog }) => (
     <div className="kpt-log--detail--header">
       {moment(kptLog.created_at).format('YYYY/MM/DD(ddd)')}
     </div>
-    <div className="kpt-log--detail--body">KPT - 内容</div>
+    <div className="kpt-log--detail--body">
+      <Pannel
+        title="Keep"
+        content={kptLog.keep}
+        additionalStyle="pannel__keep"
+      />
+    </div>
+    <div className="kpt-log--detail--body">
+      <Pannel
+        title="Problem"
+        content={kptLog.problem}
+        additionalStyle="pannel__problem"
+      />
+    </div>
+    <div className="kpt-log--detail--body">
+      <Pannel title="Try" content={kptLog.try} additionalStyle="pannel__try" />
+    </div>
   </div>
 );
 
