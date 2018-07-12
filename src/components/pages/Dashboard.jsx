3bfff98ee34c1e7a36d9bf66718b60623f86327b
Ushinji <siguma971@gmail.com>
2018-07-12 18:34:26 +0900

    サイドメニューの実装

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -2,33 +2,10 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import Queries from '../../queries';
 import Pannel from '../molecules/Pannel';
-import AddKptLogModal from '../organisms/AddKptLogModal';
+import SideMenu from '../organisms/SideMenu';
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalIsOpen: false,
-    };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-
-  openModal() {
-    this.setState({
-      modalIsOpen: true,
-    });
-  }
-
-  closeModal() {
-    this.setState({
-      modalIsOpen: false,
-    });
-  }
-
   render() {
-    const { modalIsOpen } = this.state;
-
     const kptLogItems = keptLogs => (
       <div className="kptlog">
         <div className="kptlog__header">KPT振り返り </div>
@@ -46,21 +23,7 @@ class Dashboard extends React.Component {
       <div className="container">
         <div className="container--header">KPTLog</div>
         <div className="container--body">
-          <div className="side-menu">
-            <button
-              onClick={this.openModal}
-              className="side-menu--item"
-              type="button"
-            >
-              <AddKptLogModal
-                isOpen={modalIsOpen}
-                closeModal={this.closeModal}
-                contentLabel="AddKPTLogModal"
-              />
-              モーダルを開く
-            </button>
-            <div className="side-menu--item">作成履歴</div>
-          </div>
+          <SideMenu />
           <Query query={Queries.GET_KPT_LOGS}>
             {({ loading, error, data }) => {
               if (loading) return <p>Loading...</p>;
